e7341268fe8c913877d87168fee3ad42f6db47bd
94CJG <krsoi9412@gmail.com>
2023-06-24 01:22:28 +0900

    노마더코더 선생님 Movie App Part 진행중이며,  'async' 'await' 키워드 사용했다.
    ㅁ useState와 useEffect를 복습했다.
    ㅁ API 호출을 해서 .then( )를 사용 했었지만, 'async' 'await'키워드를 이용했다.
    ㅁ 위에 2가지의 키워드를 사용한 이유는 가독성을 높일수 있다.
    ㅁ 2가지의 키워드는 비동기식 처리를 동기처리로 사용하기 위해 있는 것이며,
    promise객체를 처리 해줄수 있다.
    ㅁ async function () {} 이런식으로 사용 하고, await '비동기(promise객체 반환) 함수 사용'
    ㅁ 두개의 키워드는 짝궁이며 async () => { await fetch(''); }; 이런식으로 사용 가능하다.
    ㅁ 변수를 만들어서 변수 안에 함수를 넣고 JSON( ) 으로 변환 할 수 있다.

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log(movies);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((item) => (
+            <div key={item.id}>
+              <img src={item.medium_cover_image} alt='중앙이미지'/>
+              <h2>{item.title}</h2>
+              <p>{item.summary}</p>
+              <ul>
+                {item.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+export default App;
